de6a6bf9c410ded2a9abfe01faf71b5d56c6eeba
phenixiii <phenixiii@126.com>
2023-07-28 10:42:46 +0800

    20230504 1，提供Phenix.Core工程，精简版开发工具库 2，提供Phenix.Algorithm工程，已完成“栈式库位挑货问题”（StackStorageLocation）、“基础统计.堆积数值的众数”（ElementaryStatistics.AccumulatedMode）

---
+++ src/Phenix.StorageAlgorithm/StackStorageLocation/IGoods.cs
@@ -0,0 +1,36 @@
+﻿namespace Phenix.StorageAlgorithm.StackStorageLocation
+{
+    /// <summary>
+    /// 货物接口
+    /// </summary>
+    public interface IGoods
+    {
+        #region 属性
+        /// <summary>
+        /// ID
+        /// </summary>
+        long Id { get; }
+
+        /// <summary>
+        /// 货主
+        /// </summary>
+        string Owner { get; }
+
+        /// <summary>
+        /// 重量
+        /// </summary>
+        int Weight { get; }
+
+        /// <summary>
+        /// 排
+        /// </summary>
+        int Row { get; }
+
+        /// <summary>
+        /// 层（小数在底层）
+        /// </summary>
+        int Layer { get; }
+
+        #endregion
+    }
+}
