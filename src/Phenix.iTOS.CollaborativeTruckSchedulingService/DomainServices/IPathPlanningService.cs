11eb5d27e08100503569e7d5dc88c4fdcbd88931
phenixiii <phenixiii@126.com>
2024-10-17 09:48:09 +0800

    迁移到gitee

--- src/Phenix.iTOS.CollaborativeTruckSchedulingService/DomainServices/IPathPlanningService.cs
+++
@@ -1,7 +0,0 @@
-namespace Phenix.iTOS.CollaborativeTruckSchedulingService.DomainServices;
-
-public interface IPathPlanningService
-{
-    int DetermineSpeedingViolationInKmh(DateTime entryTimestamp, DateTime exitTimestamp);
-    string GetRoadId();
-}
--- src/Phenix.iTOS.CollaborativeTruckSchedulingService/DomainServices/ITaskDispatchService.cs
+++
@@ -1,7 +0,0 @@
-namespace Phenix.iTOS.CollaborativeTruckSchedulingService.DomainServices;
-
-public interface ITaskDispatchService
-{
-    int DetermineSpeedingViolationInKmh(DateTime entryTimestamp, DateTime exitTimestamp);
-    string GetRoadId();
-}
